2d23a985494beb96eb162a6d1fed9b3440d9b841
340Teerasak <teerasakkaewpha@gmail.com>
2024-09-26 02:50:09 +0700

    commit

---
+++ src/productsdata.js
@@ -0,0 +1,99 @@
+export const products =
+[
+  {
+    "Product-Name": "Fantastic Steel Towels",
+    "Price": "458.00",
+    "detail": "The Nagasaki Lander is the trademarked name of several series of Nagasaki sport bikes, that started with the 1984 ABC800J",
+    "img": "https://loremflickr.com/640/480/food?randomfood1",
+    "stock": "3",
+    "id": "1"
+  },
+  {
+    "Product-Name": "Recycled Metal Towels",
+    "Price": "238.00",
+    "detail": "New ABC 13 9370, 13.3, 5th Gen CoreA5-8250U, 8GB RAM, 256GB SSD, power UHD Graphics, OS 10 Home, OS Office A & J 2016",
+    "img": "https://loremflickr.com/640/480/food?randomfood2",
+    "stock": "8",
+    "id": "2"
+  },
+  {
+    "Product-Name": "Ergonomic Cotton Bike",
+    "Price": "334.00",
+    "detail": "The Nagasaki Lander is the trademarked name of several series of Nagasaki sport bikes, that started with the 1984 ABC800J",
+    "img": "https://loremflickr.com/640/480/food?randomfood3",
+    "stock": "9",
+    "id": "3"
+  },
+  {
+    "Product-Name": "Awesome Fresh Computer",
+    "Price": "673.00",
+    "detail": "The slim & simple Maple Gaming Keyboard from Dev Byte comes with a sleek body and 7- Color RGB LED Back-lighting for smart functionality",
+    "img": "https://loremflickr.com/640/480/food?randomfood4",
+    "stock": "5",
+    "id": "4"
+  },
+  {
+    "Product-Name": "Recycled Rubber Cheese",
+    "Price": "684.00",
+    "detail": "The automobile layout consists of a front-engine design, with transaxle-type transmissions mounted at the rear of the engine and four wheel drive",
+    "img": "https://loremflickr.com/640/480/food?randomfood5",
+    "stock": "1",
+    "id": "5"
+  },
+  {
+    "Product-Name": "Small Frozen Salad",
+    "Price": "765.00",
+    "detail": "The beautiful range of Apple Naturalé that has an exciting mix of natural ingredients. With the Goodness of 100% Natural Ingredients",
+    "img": "https://loremflickr.com/640/480/food?randomfood6",
+    "stock": "4",
+    "id": "6"
+  },
+  {
+    "Product-Name": "Recycled Bronze Salad",
+    "Price": "630.00",
+    "detail": "The automobile layout consists of a front-engine design, with transaxle-type transmissions mounted at the rear of the engine and four wheel drive",
+    "img": "https://loremflickr.com/640/480/food?randomfood7",
+    "stock": "2",
+    "id": "7"
+  },
+  {
+    "Product-Name": "Small Bronze Pants",
+    "Price": "399.00",
+    "detail": "The Nagasaki Lander is the trademarked name of several series of Nagasaki sport bikes, that started with the 1984 ABC800J",
+    "img": "https://loremflickr.com/640/480/food?randomfood8",
+    "stock": "3",
+    "id": "8"
+  },
+  {
+    "Product-Name": "Handcrafted Metal Chair",
+    "Price": "438.00",
+    "detail": "The slim & simple Maple Gaming Keyboard from Dev Byte comes with a sleek body and 7- Color RGB LED Back-lighting for smart functionality",
+    "img": "https://loremflickr.com/640/480/food?randomfood9",
+    "stock": "2",
+    "id": "9"
+  },
+  {
+    "Product-Name": "Electronic Soft Hat",
+    "Price": "610.00",
+    "detail": "The slim & simple Maple Gaming Keyboard from Dev Byte comes with a sleek body and 7- Color RGB LED Back-lighting for smart functionality",
+    "img": "https://loremflickr.com/640/480/food?randomfood10",
+    "stock": "5",
+    "id": "10"
+  },
+  {
+    "Product-Name": "Luxurious Cotton Car",
+    "Price": "218.00",
+    "detail": "Ergonomic executive chair upholstered in bonded black leather and PVC padded seat and back for all-day comfort and support",
+    "img": "https://loremflickr.com/640/480/food?randomfood11",
+    "stock": "1",
+    "id": "11"
+  },
+  {
+    "Product-Name": "Recycled Fresh Pants",
+    "Price": "978.00",
+    "detail": "Ergonomic executive chair upholstered in bonded black leather and PVC padded seat and back for all-day comfort and support",
+    "img": "https://loremflickr.com/640/480/food?randomfood12",
+    "stock": "2",
+    "id": "12"
+  }
+]
